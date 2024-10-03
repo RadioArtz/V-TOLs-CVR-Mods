ad4bedd4f64c8d1f66457114804716ca11fe26c0
Mathias Lefebre <54477532+RadioArtz@users.noreply.github.com>
2024-10-03 19:14:38 +0200

    initial SlimeCVR mod upload
    A mod that allows resetting & pausing slimevr tracking from your quickmenu.

---
+++ SlimeCVR/Main.cs
@@ -0,0 +1,70 @@
+﻿using MelonLoader;
+using BTKUILib.UIObjects;
+using WindowsInput;
+using WindowsInput.Native;
+
+namespace SlimeCVR
+{
+    public static class BuildInfo
+    {
+        public const string Name = "SlimeCVR";
+        public const string Description = "A mod that allows resetting & pausing slimevr tracking from your quickmenu!";
+        public const string Author = "V-TOL";
+        public const string Company = null;
+        public const string Version = "0.5.0";
+        public const string DownloadLink = null;
+    }
+
+    public class SlimeMod : MelonMod
+    {
+        InputSimulator inputSim = new InputSimulator();
+        public override void OnInitializeMelon()
+        {
+            InitializeUI();
+        }
+
+        private void InitializeUI()
+        {
+            BTKUILib.QuickMenuAPI.PrepareIcon("SlimeCVR", "TempIcon", null);
+
+            var settingsPage = new Page("SlimeCVR", "SlimeVR utils", true, null)
+            {
+                MenuTitle = "SlimeVR Utilities",
+                MenuSubtitle = "Allows resetting & pausing your slimevr tracking!"
+            };
+
+            Category trackingSettings = settingsPage.AddCategory("Tracking");
+            trackingSettings.AddButton("Yaw Reset", null, null).OnPress += () => { SendKeybind(EKeybind.yawReset); };
+            trackingSettings.AddButton("Full Reset", null, null).OnPress += () => { SendKeybind(EKeybind.fullReset); };
+            trackingSettings.AddButton("Pause Tracking", null, null).OnPress += () => { SendKeybind(EKeybind.pauseTracking); };
+            trackingSettings.AddButton("Mounting Reset", null, null).OnPress += () => { SendKeybind(EKeybind.mountingReset); };
+        }
+
+        private void SendKeybind(EKeybind bind)
+        {
+            switch (bind)
+            {
+                case EKeybind.yawReset:
+                    inputSim.Keyboard.ModifiedKeyStroke(new[] { VirtualKeyCode.CONTROL, VirtualKeyCode.MENU, VirtualKeyCode.SHIFT }, VirtualKeyCode.VK_U);
+                    break;
+                case EKeybind.fullReset:
+                    inputSim.Keyboard.ModifiedKeyStroke(new[] { VirtualKeyCode.CONTROL, VirtualKeyCode.MENU, VirtualKeyCode.SHIFT }, VirtualKeyCode.VK_Y);
+                    break;
+                case EKeybind.pauseTracking:
+                    inputSim.Keyboard.ModifiedKeyStroke(new[] { VirtualKeyCode.CONTROL, VirtualKeyCode.MENU, VirtualKeyCode.SHIFT }, VirtualKeyCode.VK_O);
+                    break;
+                case EKeybind.mountingReset:
+                    inputSim.Keyboard.ModifiedKeyStroke(new[] { VirtualKeyCode.CONTROL, VirtualKeyCode.MENU, VirtualKeyCode.SHIFT }, VirtualKeyCode.VK_I);
+                    break;
+            }
+        }
+
+        private enum EKeybind
+        {
+            yawReset = 0,
+            fullReset = 1,
+            pauseTracking = 2,
+            mountingReset = 3
+        }
+    }
+}
---
+++ SlimeCVR/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using MelonLoader;
+using SlimeCVR;
+
+[assembly: AssemblyTitle(SlimeCVR.BuildInfo.Description)]
+[assembly: AssemblyDescription(SlimeCVR.BuildInfo.Description)]
+[assembly: AssemblyCompany(SlimeCVR.BuildInfo.Company)]
+[assembly: AssemblyProduct(SlimeCVR.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + SlimeCVR.BuildInfo.Author)]
+[assembly: AssemblyTrademark(SlimeCVR.BuildInfo.Company)]
+[assembly: AssemblyVersion(SlimeCVR.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(SlimeCVR.BuildInfo.Version)]
+[assembly: MelonInfo(typeof(SlimeCVR.SlimeMod), SlimeCVR.BuildInfo.Name, SlimeCVR.BuildInfo.Version, SlimeCVR.BuildInfo.Author, SlimeCVR.BuildInfo.DownloadLink)]
+[assembly: MelonColor()]
+
+// Create and Setup a MelonGame Attribute to mark a Melon as Universal or Compatible with specific Games.
+// If no MelonGame Attribute is found or any of the Values for any MelonGame Attribute on the Melon is null or empty it will be assumed the Melon is Universal.
+// Values for MelonGame Attribute can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
+[assembly: MelonGame(null, null)]
